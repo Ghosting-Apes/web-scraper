ba4d93f24b425920d5cab7e2fbfc8ee84cd0b72e
cage wellman <cagewellman@gmail.com>
2023-08-02 15:22:43 -0400

    implemented run command for multiple searches

--- cli.py
+++ cli.py
@@ -1,15 +1,35 @@
 import typer
 from typing_extensions import Annotated
 
+state = {"hasCommand": False}
 app = typer.Typer()
         
 @app.command()
-def search(phrase: Annotated[str, typer.Argument(help="Input the phrase to be scraped.")], location: Annotated[str, typer.Argument(help="Input source to scrape (e.g. Wikipedia)")] = "Google"):
+def search(phrase: Annotated[str, typer.Argument(help="Input the phrase to be scraped.")], 
+           location: Annotated[str, typer.Argument(help="Input source to scrape (e.g. Wikipedia)")] = "Google"):
     print(f"Searching for {phrase} within {location}...")
         
+        
+@app.command()
+def run(hasCommand: bool = True):
+        print(">> ", end="")
+        inp = input()
+        inpList = inp.split("\"")[1:]
+        inpList[1] = inpList[1].strip()
+        print(inpList)
+        while hasCommand:
+             search(inpList[0], inpList[1])
+             print(">> ", end="")
+             inp = input()
+             if inp == "quit":
+                  break
+             inpList = inp.split("\"")[1:]
+             inpList[1] = inpList[1].strip()
+
 @app.callback()
 def main():
     pass
         
 if __name__ == "__main__":
     app()
+
